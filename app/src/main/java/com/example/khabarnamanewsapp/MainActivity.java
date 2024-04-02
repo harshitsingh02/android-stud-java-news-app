f2ae74b72d269bb252826115df20d518bb9fd5e1
Ankit Panigrahi <panigrahi0702@gmail.com>
2024-04-03 01:26:34 +0530

    Till 2nd Video (Display Nws RecyclerView)

---
+++ app/src/main/java/com/example/khabarnamanewsapp/MainActivity.java
@@ -0,0 +1,92 @@
+package com.example.khabarnamanewsapp;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.progressindicator.LinearProgressIndicator;
+import com.kwabenaberko.newsapilib.NewsApiClient;
+import com.kwabenaberko.newsapilib.models.request.TopHeadlinesRequest;
+import com.kwabenaberko.newsapilib.models.response.ArticleResponse;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.kwabenaberko.newsapilib.models.Article;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    List<Article> articleList = new ArrayList<>();
+    NewsRecyclerAdapter adapter;
+    LinearProgressIndicator progressIndicator;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.news_recycler_view);
+        progressIndicator = findViewById(R.id.progress_bar);
+
+        setupRecyclerView();
+        getNews();
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    void setupRecyclerView(){
+        recyclerView.setLayoutManager((new LinearLayoutManager(this)));
+        adapter = new NewsRecyclerAdapter(articleList);
+        recyclerView.setAdapter(adapter);
+    }
+
+    void changeInProgress(boolean show)
+    {
+        if(show)
+            progressIndicator.setVisibility(View.VISIBLE);
+        else
+            progressIndicator.setVisibility(View.INVISIBLE);
+    }
+
+    void getNews()
+    {
+        changeInProgress(true);
+        NewsApiClient newsApiClient = new NewsApiClient("b7a500efaa8f4a7c981bdcc643d0ddc2");
+        newsApiClient.getTopHeadlines(
+                new TopHeadlinesRequest.Builder()
+                        .language("en")
+                        .build(),
+                new NewsApiClient.ArticlesResponseCallback() {
+                    @Override
+                    public void onSuccess(ArticleResponse response) {
+//                        response.getArticles().forEach((a) -> {
+//                            Log.i("ARTICLE", a.getTitle());
+//                        });
+                        runOnUiThread(()->{
+                            changeInProgress(false);
+                            articleList = response.getArticles();
+                        });
+                    }
+
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                        Log.i("GOT FAILURE", throwable.getMessage());
+                    }
+                }
+        );
+    }
+}
---
+++ app/src/main/java/com/example/khabarnamanewsapp/NewsRecyclerAdapter.java
@@ -0,0 +1,65 @@
+package com.example.khabarnamanewsapp;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.view.View;
+
+import java.util.List;
+
+import com.kwabenaberko.newsapilib.models.Article;
+import com.squareup.picasso.Picasso;
+
+public class NewsRecyclerAdapter extends RecyclerView.Adapter<NewsRecyclerAdapter.NewsViewHolder> {
+
+    List<Article> articleList;
+    NewsRecyclerAdapter(List<Article> articleList)
+    {
+        this.articleList = articleList;
+    }
+
+    @NonNull
+    @Override
+    public NewsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.news_recycler_row,parent,false);
+        return new NewsViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NewsViewHolder holder, int position) {
+        Article article = articleList.get(position);
+        holder.titleTextView.setText(article.getTitle());
+        holder.sourceTextView.setText(article.getSource().getName());
+        Picasso.get().load(article.getUrlToImage())
+                .error(R.drawable.no_image_icon)
+                .placeholder(R.drawable.no_image_icon)
+                .into(holder.imageView);
+    }
+
+    void updateDate(){
+        articleList.clear();
+        articleList.addAll(data);
+    }
+
+    @Override
+    public int getItemCount() {
+        return articleList.size();
+    }
+
+    class NewsViewHolder extends RecyclerView.ViewHolder{
+        TextView titleTextView, sourceTextView;
+        ImageView imageView;
+
+        public NewsViewHolder(View itemView) {
+            super(itemView);
+            titleTextView = itemView.findViewById(R.id.article_title);
+            sourceTextView = itemView.findViewById(R.id.article_source);
+            imageView = itemView.findViewById(R.id.article_image_view);
+        }
+
+    }
+}
