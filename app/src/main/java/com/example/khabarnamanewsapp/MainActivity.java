253e95d1e7a999639262c2f369eb3ca6ff67c796
Ankit Panigrahi <panigrahi0702@gmail.com>
2024-04-03 02:13:31 +0530

    Till 2nd Video (Display Nws RecyclerView)

--- app/src/main/java/com/example/khabarnamanewsapp/MainActivity.java
+++ app/src/main/java/com/example/khabarnamanewsapp/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.khabarnamanewsapp;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -22,12 +24,16 @@ import java.util.List;
 import com.kwabenaberko.newsapilib.models.Article;
 
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     RecyclerView recyclerView;
     List<Article> articleList = new ArrayList<>();
     NewsRecyclerAdapter adapter;
     LinearProgressIndicator progressIndicator;
+    Button btn1, btn2, btn3, btn4, btn5, btn6, btn7;
+
+    SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,9 +43,36 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.news_recycler_view);
         progressIndicator = findViewById(R.id.progress_bar);
+        searchView = findViewById(R.id.search_view);
+
+        btn1 = findViewById(R.id.btn_1);
+        btn2 = findViewById(R.id.btn_2);
+        btn3 = findViewById(R.id.btn_3);
+        btn4 = findViewById(R.id.btn_4);
+        btn5 = findViewById(R.id.btn_5);
+        btn6 = findViewById(R.id.btn_6);
+        btn7 = findViewById(R.id.btn_7);
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+        btn4.setOnClickListener(this);
+        btn6.setOnClickListener(this);
+        btn7.setOnClickListener(this);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                getNews("GENERAL", query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
 
         setupRecyclerView();
-        getNews();
+        getNews("GENERAL", null);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -62,13 +95,15 @@ public class MainActivity extends AppCompatActivity {
             progressIndicator.setVisibility(View.INVISIBLE);
     }
 
-    void getNews()
+    void getNews(String category, String query)
     {
         changeInProgress(true);
         NewsApiClient newsApiClient = new NewsApiClient("b7a500efaa8f4a7c981bdcc643d0ddc2");
         newsApiClient.getTopHeadlines(
                 new TopHeadlinesRequest.Builder()
                         .language("en")
+                        .category(category)
+                        .q(query)
                         .build(),
                 new NewsApiClient.ArticlesResponseCallback() {
                     @Override
@@ -90,5 +125,12 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
         );
+    }
+
+    @Override
+    public void onClick(View v) {
+        Button btn = (Button) v;
+        String category = btn.getText().toString();
+        getNews(category,null);
     }
 }
